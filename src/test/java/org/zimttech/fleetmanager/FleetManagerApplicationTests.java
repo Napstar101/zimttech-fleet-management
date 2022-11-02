e9258b498ba630f1edcc12760a821c217c0cd21a
Jabulani Nkomo <jabulanironald7@gmail.com>
2022-11-02 10:56:50 +0200

    first commit

---
+++ src/test/java/org/zimttech/fleetmanager/FleetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zimttech.fleetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
